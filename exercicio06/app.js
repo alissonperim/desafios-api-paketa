6427d744a85c90821b7fb56d1dff364df8047347
Alisson Perim Honostorio <alissonhonostorio@gmail.com>
2021-07-01 14:15:07 -0300

    Finalizado todos os exercícios

--- exercicio06/app.js
+++ exercicio06/app.js
@@ -10,33 +10,31 @@ app.get('/infos', (req, res) => {
 
 app.post('/infos', (req, res) => {
     let { max_velocity_allowed, car_velocity } = req.body
-    let velocity_exceeded = 0
 
     const info = [
         {
             traffic_ticket: true,
-            velocity__exceeded: velocity_exceeded,
-            percentage_exceeded: "10%"
+            velocity__exceeded: car_velocity,
+            percentage_exceeded: 0
         }
     ]
 
     const carVelocityExceeded = (velocity) => {
-        if (car_velocity > max_velocity_allowed) {
-            let percentageExceedded = ((car_velocity - max_velocity_allowed) / max_velocity_allowed) * 100
-            if (percentageExceedded >= 10)
-                info.map(i => (
-                    i.traffic_ticket = true,
-                    i.velocity__exceeded = velocity,
-                    i.percentage_exceeded = percentageExceedded
-                ))
+        let percentageExceedded = ((car_velocity - max_velocity_allowed) / max_velocity_allowed) * 100
+        if (percentageExceedded >= 10) {
+            info.map(i => (
+                i.traffic_ticket = true,
+                i.velocity__exceeded = velocity,
+                i.percentage_exceeded = percentageExceedded.toFixed(2)
+            ))
             res.json(info)
         } else {
             info.map(i => (
                 i.traffic_ticket = false,
-                i.velocity__exceeded = 0,
-                i.percentage_exceeded = 0
+                i.velocity__exceeded = velocity,
+                i.percentage_exceeded = percentageExceedded.toFixed(2)
             ))
-            res.json()
+            res.json(info)
         }
     }
     carVelocityExceeded(car_velocity)
