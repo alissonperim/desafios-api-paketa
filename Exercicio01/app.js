3d33f1318877a8c60b08c213bd294cba43199a7f
Alisson Perim Honostorio <alissonhonostorio@gmail.com>
2021-07-02 13:31:03 -0300

    Adicionado appReturnToFront, feito a nivel de conhecimento, para retornar dados da API para um front-end

--- Exercicio01/app.js
+++ Exercicio01/app.js
@@ -8,18 +8,4 @@ const fetchPokemonByAxios = () => {
         })
 }
 
-// const fetchPokemon = () => {
-//     const url = `https://pokeapi.co/api/v2/pokemon/25`
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(pokemon => {
-//             const body = document.querySelector('body')
-//             body.innerHTML += `
-//                 <h2>O seu pokemon é: <span>${pokemon.name}<span></h2>
-//             `
-//             console.log(pokemon)
-//         })
-// }
-
-
 fetchPokemonByAxios()
